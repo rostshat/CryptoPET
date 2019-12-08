d02ce811ea0d3812a925d7dc29576a69215d012d
Rostyslav <rost.shat@gmail.com>
2019-12-08 20:32:30 +0200

    Header

---
+++ header.php
@@ -0,0 +1,37 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Crypto Exchange</title>
+    
+    <link rel="stylesheet" href="css/index.css">
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="container">
+           <div class="row">
+                <div id="header__logo" class="col-md-3">
+                    <img src="assets/images/logo.png" alt="Logotype">
+                </div>
+                <div id="header__menu" class="col-md-6">
+                    <div class="row menu">
+                        <div class="menu__item col-md-3">Buy</div>
+                        <div class="menu__item col-md-3">Sell</div>
+                        <div class="menu__item col-md-3">Contacts</div>
+                        <div class="menu__item col-md-3">About</div>
+                    </div>
+                </div>
+                <div id="header__user" class="col-md-3">
+                    <div class="buttons">
+                        <button id="button__login">
+                            Login
+                        </button>
+                        <button id="button__register">
+                            Register
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    include 'header.php';
+?>
+
+<section id="promo_buy">
+    <div class="container">
+        
+    </div>
+</section>
