76543902f49e460a1d9f39b2140cbd3d98e5360a
Rostyslav <rost.shat@gmail.com>
2019-12-09 06:54:10 +0200

    Form 0.1v

--- index.php
+++ index.php
@@ -7,8 +7,17 @@
         <div class="row">
             <div class="col-md-6 col-sm-12 form__buy">
                 <h2>I want to <span class="yellow">Buy</span></h2>
-                <form action="">
-                    <input type="text">
+                <form action="#">
+                    <input type="text" class="form-control" value="1" min="0.0001" max="10">
+                        <div class="input-group-btn-vertical">
+                        <button class="btn btn-default" type="button">
+                            <i class="fa fa-caret-up"></i>
+                        </button>
+                        <button class="btn btn-default" type="button">
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                    </div>
+                    <p class="help-block">Min 0 - Max 5.</p>
                 </form>
             </div>
             <div class="col-md-5 col-sm12">
