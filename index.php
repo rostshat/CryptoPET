d97f31ba708ad3c14f3642ff2ae8259967d7f9b0
Rostyslav <rost.shat@gmail.com>
2019-12-09 06:07:30 +0200

    Action Buy Form

--- index.php
+++ index.php
@@ -4,6 +4,19 @@
 
 <section id="promo_buy">
     <div class="container">
-        
+        <div class="row">
+            <div class="col-md-6 col-sm-12 form__buy">
+                <h2>I want to <span class="yellow">Buy</span></h2>
+                <form action="">
+                    <input type="text">
+                </form>
+            </div>
+            <div class="col-md-5 col-sm12">
+                
+            </div>
+            <div class="col-md-1">
+                
+            </div>
+        </div>
     </div>
 </section>
