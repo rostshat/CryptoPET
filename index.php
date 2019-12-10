cdbec70034457b922b9158bd1d3f7eab924ccbe1
Rostyslav <rost.shat@gmail.com>
2019-12-10 20:27:57 +0200

    Banner

--- index.php
+++ index.php
@@ -9,22 +9,15 @@
                 <h2>I want to <span class="yellow">Buy</span></h2>
                 <form action="#">
                     <input type="text" class="form-control" value="1" min="0.0001" max="10">
-                        <div class="input-group-btn-vertical">
-                        <button class="btn btn-default" type="button">
-                            <i class="fa fa-caret-up"></i>
-                        </button>
-                        <button class="btn btn-default" type="button">
-                            <i class="fa fa-caret-down"></i>
-                        </button>
-                    </div>
-                    <p class="help-block">Min 0 - Max 5.</p>
+                    
+                    <p class="help-block">Min 0.0001 - Max 10</p>
                 </form>
             </div>
-            <div class="col-md-5 col-sm12">
-                
+            <div class="col-md-5 col-sm12 banner_promo">
+                                <img src="assets/images/banner_phone.png" alt="Banner" class="banner">
             </div>
             <div class="col-md-1">
-                
+
             </div>
         </div>
     </div>
