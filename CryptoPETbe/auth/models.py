a88ba394a9982e0a1cf3b05437055416325846ce
DimaTheCat <skr1ng4m1ng@gmail.com>
2019-12-09 18:07:49 -0600

    Add db model

--- CryptoPETbe/auth/models.py
+++ CryptoPETbe/auth/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+#МИГРАЦИЯ НЕ СДЕЛАНА
+
+class Users(models.Model):
+    name = models.CharField(max_length=20)
+    password = models.CharField()
+    email = models.CharField()
+    balance = models.IntegerField()
