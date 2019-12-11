87196ab81d8c11519c253795dd95eb03bef14ad9
DimaTheCat <skr1ng4m1ng@gmail.com>
2019-12-10 18:53:32 -0600

    Add db model

--- CryptoPETbe/user_auth/models.py
+++ CryptoPETbe/user_auth/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class user_acc(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    password = models.CharField(max_length=200)
+    balance = models.FloatField()
