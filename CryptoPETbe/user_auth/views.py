93716d6dbe93e1f0aa45840afc4f3a7016708905
DimaTheCat <skr1ng4m1ng@gmail.com>
2019-12-11 19:02:30 -0600

    Add app to url (not working)

--- CryptoPETbe/user_auth/views.py
+++ CryptoPETbe/user_auth/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("This is a test page!")
+
+# def userauth(request):
+#     html = "<html><body>It is test page.</body></html>"
+#     return HttpResponse(html)
