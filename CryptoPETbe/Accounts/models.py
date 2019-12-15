739d6332a61d281daafa9e4e68e88b4a2df37344
DimaTheCat <skr1ng4m1ng@gmail.com>
2019-12-15 10:05:04 -0600

    Create db model

--- CryptoPETbe/Accounts/models.py
+++ CryptoPETbe/Accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Accs(models.Model):
+    login = models.CharField(max_length=20)
+    password = models.CharField(max_length=50)
+    USD = models.FloatField()
+    BTC = models.FloatField()
