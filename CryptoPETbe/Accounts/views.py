aad0d347b9bff7c58ba869c1aadf4328b0b42afa
DimaTheCat <skr1ng4m1ng@gmail.com>
2019-12-18 17:55:09 -0600

    Add login check

--- CryptoPETbe/Accounts/views.py
+++ CryptoPETbe/Accounts/views.py
@@ -4,16 +4,25 @@ from .models import Accs
 
 def RegAndLogin(request):
     if request.method == "POST":
-        if request.REGISTER.get('regpass') == request.REGISTER.get('regpassv'):
-            accs = Accs()
-            accs.login = request.REGISTER.get('reglogin')
-            accs.password = request.REGISTER.get('regpass')
-            accs.USD = 0
-            accs.BTC = 0
-            accs.save()
+        if request.POST.get('regpass') and request.POST.get('regpassv'): #==
+            if request.POST.get('regpass') == request.POST.get('regpassv'):
+                accs = Accs()
+                accs.login = request.POST.get('reglogin')
+                accs.password = request.POST.get('regpass')
+                accs.USD = 0
+                accs.BTC = 0
+                accs.save()
 
-            html = "<html><body>Registration sucsessful!</body></html>"
-            return HttpResponse(html)
+                return HttpResponse('Registration sucsessful!')
 
+            else:
+                return HttpResponse('Error!')
+
+        elif request.POST.get('loginlogin') and request.POST.get('loginpass'):
+                accs = Accs()
+                if accs.objects.get(login=request.POST.get('loginlogin')) == request.POST.get('loginlogin') and accs.objects.get(login=request.POST.get('loginpass')) == request.POST.get('loginpass'):
+                    HttpResponse("Authorisation sucsessful!")
+        
         else:
-            pass
+            return HttpResponse('Error!')
+        
