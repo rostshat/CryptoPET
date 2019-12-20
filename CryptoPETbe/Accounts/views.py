cb99005f49cab7166c23f29a96acc1cb84fb360d
DimaTheCat <skr1ng4m1ng@gmail.com>
2019-12-19 20:55:16 -0600

    Add check if login already exists

--- CryptoPETbe/Accounts/views.py
+++ CryptoPETbe/Accounts/views.py
@@ -6,17 +6,21 @@ def RegAndLogin(request):
     if request.method == "POST":
         if request.POST.get('regpass') and request.POST.get('regpassv'): #==
             if request.POST.get('regpass') == request.POST.get('regpassv'):
-                accs = Accs()
-                accs.login = request.POST.get('reglogin')
-                accs.password = request.POST.get('regpass')
-                accs.USD = 0
-                accs.BTC = 0
-                accs.save()
+                login = request.POST.get('reglogin')
+                if Accs.objects.filter(login=login).exists():
+                    return HttpResponse('Error!')
+                else:
+                    accs = Accs()
+                    accs.login = request.POST.get('reglogin')
+                    accs.password = request.POST.get('regpass')
+                    accs.USD = 0
+                    accs.BTC = 0
+                    accs.save()
 
-                return HttpResponse('Registration sucsessful!')
-
+                    return HttpResponse('Registration sucsessful!')
             else:
                 return HttpResponse('Error!')
+            
 
         elif request.POST.get('loginlogin') and request.POST.get('loginpass'):
                 login = request.POST.get('loginlogin')
